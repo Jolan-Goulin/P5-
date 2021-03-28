0955bfd5bf45ce1555a2a2f45b0f33c6f4a7a93c
Jolan-Goulin <77050754+Jolan-Goulin@users.noreply.github.com>
2021-03-28 20:18:10 +0200

    Add files via upload

--- p5Template-master/src/sketch.ts
+++ p5Template-master/src/sketch.ts
@@ -4,22 +4,28 @@
 
 const gui = new dat.GUI()
 const params = {
-    y: 20,
-    n: 250,
-    b: 400,
-    h: 1.25,
-    s: 1,
-    s2: 1,
-    c: 1,
+    etages: 20,
+    rotation: 250,
+    ScaleAjustement: 400,
+    nombreElipses: 1.25,
+    CoefDeDiminutionEtages: 1,
+    hauteurElipses: 1,
+    randomSeed: 0,
+    color : 1,
+    sensRota : 1,
+    MethodRandom : 0,
     Download_Image: () => save(),
 }
-gui.add(params, "n", 0, 500, 1)
-gui.add(params, "b", 0, 500, 1)
-gui.add(params, "y", 0, 60, 1)
-gui.add(params, "h", 1.25  , 5, 0.05)
-gui.add(params, "s", 0  , 1, 0.05)
-gui.add(params, "s2", 0  , 2, 0.05)
-gui.add(params, "c", 0  , 100, 0.05)
+gui.add(params, "rotation", 0, 500, 1)
+gui.add(params, "ScaleAjustement", 0, 500, 1)
+gui.add(params, "etages", 0, 60, 1)
+gui.add(params, "nombreElipses", 1.25  , 5, 0.05)
+gui.add(params, "CoefDeDiminutionEtages", 0  , 1, 0.05)
+gui.add(params, "hauteurElipses", 0  , 2, 0.05)
+gui.add(params, "randomSeed", 0  , 100, 1)
+gui.add(params, "MethodRandom", 0  , 3, 1)
+gui.add(params, "color", 0  , 1, 1)
+gui.add(params, "sensRota", -1  , 1, 1)
 gui.add(params, "Download_Image")
 
 // -------------------
@@ -31,31 +37,39 @@ function draw() {
     rectMode(CENTER)
     translate(width/2,height/2)
     let i =0;
-    for (i =0; i<params.y; i++){
+    if (params.MethodRandom == 0 ){
+        randomSeed(params.randomSeed)}
+    for (i =0; i<params.etages; i++){
         fill("black")
-        rotate(-i* PI/(2*params.n)/i*2)
+        rotate(params.sensRota*(-i)* PI/(2*params.rotation)/i*2)
         rect(0,0,width-100,height-100)
-        scale(0.9*params.s,0.9*params.s,1) 
-
-        for(let j = 0; j<32*params.h; j++){
+        scale(0.9*params.CoefDeDiminutionEtages,0.9*params.CoefDeDiminutionEtages,1) 
+        if (params.MethodRandom == 1 ){
+            randomSeed(params.randomSeed)}
+        for(let j = 0; j<32*params.nombreElipses; j++){
+            if (params.MethodRandom == 2 ){
+                randomSeed(params.randomSeed)}
             fill("white")
+            if (params.color == 1 ){
+                fill(random(1)*255,random(1)*255,random(1)*255,255)
+            }
             
-            let alpha = (j*PI/(16*params.h))%(PI/2)
+            let alpha = (j*PI/(16*params.nombreElipses))%(PI/2)
             if ( alpha > PI/4){
                 alpha = (PI/2)-alpha
             }   
             let comp = 1/cos(alpha)
-            let redu = params.s2 * (1+alpha/params.b)
+            let redu = params.hauteurElipses * (1+alpha/params.ScaleAjustement)
             scale(redu, redu ,1)
-            ellipse((width/2-67)*comp,0,100,50+50*alpha*alpha)
+            ellipse((width/2-67)*comp,0,100+15*alpha,(60+50*alpha*alpha)/2)
             scale(1/redu, 1/redu ,1)
-            rotate(PI/(16*params.h))
+            rotate(PI/(16*params.nombreElipses))
         }
         
         
     }
     fill("white")
-    rotate(i* PI/(2*params.n)/i*2)
+    rotate(i* PI/(2*params.rotation)/i*2)
     rect(0,0,width-100,height-100)
 
 }
